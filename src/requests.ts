3ab2f926a1ecd09791d84ad8500aa076aab48ebb
KC Kai <kirstenckai@gmail.com>
2021-10-26 20:24:50 -0400

    separate api request call

---
+++ src/requests.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { API_KEY, API_URL, API_HOST, GAMES } from "./constants";
+import { sanitizeGame } from "./helpers";
+
+export const getGames = async (date: string) => {
+  return await axios({
+    url: `${API_URL}${GAMES}`,
+    method: 'get',
+    params: {
+      date,
+    },
+    headers: {
+      "Content-Type": "application/json",
+      "x-rapidapi-host": `${API_HOST}`,
+      "x-rapidapi-key": `${API_KEY}`
+    }
+  }).then(response => response.data.response.map(sanitizeGame));
+};
