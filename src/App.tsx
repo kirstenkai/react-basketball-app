6b5c3122b23414935713874e375fe95722e00f74
KC Kai <kirstenckai@gmail.com>
2021-10-26 20:28:37 -0400

    add main app with state and request handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import MatchTable from "./components/MatchesTable";
+
+import "./App.css";
+import { getGames } from "./requests";
+
+type Game = {
+  id: string;
+  country: string;
+  date: string;
+  league: string;
+  homeTeam: string;
+  awayTeam: string;
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	// assign match information
+	const [matchData, setMatchData] = React.useState<Array<Game>>([]);
+
+	useEffect(() => {
+		const getMatches = async () => {
+			let tomorrow = new Date();
+      tomorrow.setDate(tomorrow.getDate() + 1);
+      let dayAfter = new Date(tomorrow);
+      dayAfter.setDate(dayAfter.getDate() + 1);
+
+      // Need to add timezone considerations
+			const tomorrowData = await getGames(tomorrow.toISOString().split("T")[0]);
+			const dayAfterData = await getGames(dayAfter.toISOString().split("T")[0]);
+
+      const matches = [...tomorrowData, ...dayAfterData];
+			setMatchData(matches);
+		};
+
+		getMatches();
+	}, []);
+
+	return (
+		<div className="App">
+			<h1>GameOn - Basketball Matches Score Predictions</h1>
+			<h2>Upcoming Matches</h2>
+			<MatchTable games={matchData}/>
+		</div>
+	);
 }
 
 export default App;
