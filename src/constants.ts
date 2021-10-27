17621041e7d543dbd72755d3be51d7cb7f42eeef
KC Kai <kirstenckai@gmail.com>
2021-10-26 20:23:03 -0400

    add env variables to constant file

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const API_URL = process.env.REACT_APP_RAPID_API_URL;
+export const API_HOST = process.env.REACT_APP_RAPID_API_HOST;
+export const API_KEY = process.env.REACT_APP_RAPID_API_KEY;
+export const GAMES = process.env.REACT_APP_GAME_ENDPOINT;
